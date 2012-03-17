c656cd8737939507400102e2141012151bf8562c
Jon Morton <jon.morton@gmail.com>
2012-03-17 06:16:43 -0400

    Switch to monokai

--- jmorton/theme.el
+++ jmorton/theme.el
@@ -1,3 +1,10 @@
+;; (vendor "emacs-color-theme-solarized")
+;; (add-to-list 'load-path "~/.emacs.d/vendor/emacs-color-theme-solarized")
+;; (load "color-theme-solarized.el")
+;; (color-theme-solarized-dark)
 
 (load "color-theme-almost-monokai.el")
 (color-theme-almost-monokai)
+
+(set-face-attribute 'default nil :height 140)
+(set-frame-parameter nil 'fullscreen 'fullboth)
