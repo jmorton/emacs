eae024194962bbc7c9e57c78c413011ae1dbf594
Jon Morton <jon.morton@gmail.com>
2013-03-02 13:43:27 -0500

    Organizing packager.

--- emacs.el
+++ emacs.el
@@ -1,3 +1,9 @@
+; link ~/.emacs to this file
+;  ln -s ~/.emacs ~/Projects/emacs/emacs.el
+; link ~/.emacs.d to the directory containing
+; this file.
+;  ln -s ~/.emacs.d ~/Projects/emacs
+
 (add-to-list 'load-path "~/.emacs.d/")
 (load "jmorton")
-(put 'downcase-region 'disabled nil)
+
--- jmorton.el
+++ jmorton.el
@@ -1,24 +1,19 @@
 (add-to-list 'load-path "~/.emacs.d/vendor")
-(add-to-list 'load-path "~/.emacs.d/vendor/solarized")
+(add-to-list 'load-path "~/.emacs.d/elpa")
 
 (setq custom-file "~/.emacs.d/jmorton/custom.el")
-
 (when (file-exists-p custom-file) (load custom-file))
 
 (load "jmorton/defaults")
 (load "jmorton/defuns")
 (load "jmorton/windows")
 (load "jmorton/theme")
-(load "jmorton/ruby")
-(load "jmorton/yaml")
-(load "jmorton/js")
 (load "jmorton/clojure")
-(load "jmorton/scala")
-(load "jmorton/haskell")
-(load "jmorton/c")
-(load "jmorton/org")
+
 
 (require 'package)
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-
+(add-to-list 'package-archives
+	     '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (package-initialize)
