6dcb182d742957dd972c586519162dae0ddac38b
Jon Morton <jmorton@core>
2013-12-08 20:59:11 +0000

    Cleaning up.

--- jmorton.el
+++ jmorton.el
@@ -4,16 +4,27 @@
 (setq custom-file "~/.emacs.d/jmorton/custom.el")
 (when (file-exists-p custom-file) (load custom-file))
 
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
+(package-initialize)
+
+(unless (package-installed-p 'color-theme)
+  (package-install 'color-theme))
+
+(unless (package-installed-p 'clojure-mode)
+  (package-install 'clojure-mode))
+
+(unless (package-installed-p 'cider)
+  (package-install 'cider))
+
+(unless (package-installed-p 'rainbow-delimiters)
+  (package-install 'rainbow-delimiters))
+
+
 (load "jmorton/defaults")
 (load "jmorton/defuns")
 (load "jmorton/windows")
 (load "jmorton/theme")
 (load "jmorton/clojure")
-
-
-(require 'package)
-(add-to-list 'package-archives
-	     '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(package-initialize)
--- jmorton/theme.el
+++ jmorton/theme.el
@@ -1,4 +1,3 @@
-
 (load "color-theme-molokai.el")
 (color-theme-molokai)
 (set-face-attribute 'default nil :height 140)
