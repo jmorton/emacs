c04298de13776fb3522dc50fec2074f076ad22a5
Jonathan Morton <jmorton@element84.com>
2014-05-14 13:19:55 -0400

    Nuclear launch detected... boom.

--- emacs.el
+++
@@ -1,9 +0,0 @@
-; link ~/.emacs to this file
-;  ln -s ~/Projects/emacs/emacs.el ~/.emacs
-; link ~/.emacs.d to the directory containing
-; this file.
-;  ln -s ~/Projects/emacs ~/.emacs.d
-
-(add-to-list 'load-path "~/.emacs.d/")
-(load "jmorton")
-
--- jmorton/theme.el
+++
@@ -1,3 +0,0 @@
-(set-face-attribute 'default nil :height 140)
-(load-theme 'molokai t)
-
